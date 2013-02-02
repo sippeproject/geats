d7aeaf5db7057b19a7a433f263baa07ec628ae3e
Robert M. Thomson <rmt-git@corporatism.org>
2013-02-02 11:33:46 +0100

    add extra information to setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,14 @@ setup(
     packages = find_packages(),
     package_data = { '': [ "*.rst", "LICENSE" ] },
     scripts = ["bin/vmctl"],
+    license = "LGPL 2.1",
+    keywords = "LXC virtualization",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: No Input/Output (Daemon)",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+    ]
 )
